660887f0603e4dbf8262b25af6e9906723b706e0
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-12-28 23:55:50 -0500

    Allow waterlogging hollow logs

--- src/main/java/io/github/haykam821/hollowlogs/HollowPillarBlock.java
+++ src/main/java/io/github/haykam821/hollowlogs/HollowPillarBlock.java
@@ -14,7 +14,16 @@ import net.minecraft.entity.EntityContext;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.Direction;
 
-public class HollowPillarBlock extends LogBlock {
+import net.minecraft.block.Waterloggable;
+import static net.minecraft.state.property.Properties.WATERLOGGED;
+import net.minecraft.state.StateManager;
+import net.minecraft.world.IWorld;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.block.Block;
+
+public class HollowPillarBlock extends LogBlock implements Waterloggable {
 	public static final VoxelShape X_SHAPE = VoxelShapes.union(
 		VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1f, 0.0625f),
 		VoxelShapes.cuboid(0f, 0f, 0.9375f, 1f, 1f, 1f),
@@ -36,6 +45,30 @@ public class HollowPillarBlock extends LogBlock {
 
 	public HollowPillarBlock(final MaterialColor endMaterialColor, Settings settings) {
 		super(endMaterialColor, settings);
+		this.setDefaultState(super.getDefaultState().with(WATERLOGGED, false));
+	}
+
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+		return super.getPlacementState(ctx).with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		super.appendProperties(builder);
+		builder.add(WATERLOGGED);
+	}
+
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
+		if (state.get(WATERLOGGED)) {
+			world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+		return super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
 	}
 
 	@Override
